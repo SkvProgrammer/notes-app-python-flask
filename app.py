503455d5a739ddaeb0a8a154bb5c2dcb9d89a6f9
Satyam Kumar Verman <satyamvarman2004@gmail.com>
2024-12-07 20:32:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,6 @@ def update_note(id):
     return jsonify({'message': 'Note updated successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Railway sets PORT in environment variables
+    app.run(host="0.0.0.0", port=port)
+    
